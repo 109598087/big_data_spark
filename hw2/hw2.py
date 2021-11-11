4dcc1cf310cbab113fb4acd203bb8176edd87062
109598087 <t109598087@ntut.org.tw>
2021-11-11 11:45:54 +0800

    ok

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -1,9 +1,4 @@
-from itertools import product
-
 import pandas as pd
-import time
-
-start_time = time.time()
 
 
 def remove_ch(word):
@@ -14,12 +9,8 @@ def remove_ch(word):
     return word
 
 
-def get_df_title_headline_list(df, title_headline):
-    return df[title_headline].values
-
-
-def sort_words_by_most_frequent_in_descending_order(df, title_headline):
-    word_list = get_df_title_headline_list(df, title_headline)
+def get_word_count_dict(df):
+    word_list = df.values
     word_count_dict = dict()
     for title in word_list:
         title = str(title)
@@ -32,152 +23,42 @@ def sort_words_by_most_frequent_in_descending_order(df, title_headline):
                 word_count_dict[word] += 1
             else:
                 word_count_dict[word] = 1
-    word_count_dict = {k: v for k, v in sorted(word_count_dict.items(), key=lambda item: item[1], reverse=True)}
-    # return word_count_dict
-    # return list(word_count_dict)[:5]
-    return list(word_count_dict)[:100]
-    # return list(word_count_dict)
+    return word_count_dict
 
 
-def print_most_frequent_words_in_title_and_headline(df, groupby_column):
-    # remove duplicate column
-    column_set_list = list(set(df[groupby_column].to_list()))
-    # print
-    for column in column_set_list:
-        date_df = df[df[groupby_column] == column]
-        print(column, 'Title', sort_words_by_most_frequent_in_descending_order(date_df, 'Title'))
-    for column in column_set_list:
-        date_df = df[df[groupby_column] == column]
-        print(column, 'Headline', sort_words_by_most_frequent_in_descending_order(date_df, 'Headline'))
+def sort_word_count_dict_in_descending_order(word_count_dict):
+    return {k: v for k, v in sorted(word_count_dict.items(), key=lambda item: item[1], reverse=True)}
 
 
-def concat_same_platform_df(platform, topic_list):
-    feedback_df = pd.read_csv('feedback/' + platform + '_' + topic_list[0] + '.csv')
-    for i in range(1, len(topic_list)):
-        feedback_df = pd.concat([feedback_df, pd.read_csv('feedback/' + platform + '_' + topic_list[i] + '.csv')],
-                                ignore_index=True)
-    feedback_df['IDLink'] = feedback_df['IDLink'].astype('int64')
-    return feedback_df
-
-
-# read csv
-df = pd.read_csv('News_Final.csv')
-
-df = df.dropna()
-
+df = pd.read_csv('File:///opt/spark/hw2/News_Final.csv')
+# change dtype
+df['SentimentTitle'] = df['SentimentTitle'].astype('float32')
+df['SentimentHeadline'] = df['SentimentHeadline'].astype('float32')
 # PublishDate to PublishDate_date column
 all_date_list = df['PublishDate'].values
 df['PublishDate_date'] = [date_time.split(' ')[0] for date_time in all_date_list]
+print(df['PublishDate_date'])
 
-df['Title_split_lower'] = [[remove_ch(title_s).lower() for title_s in title.split(' ')] for title in df['Title']]
-df['Headline_split_lower'] = [[remove_ch(title_s).lower() for title_s in title.split(' ')] for title in df['Headline']]
-
-# (1) total
-print('Title', sort_words_by_most_frequent_in_descending_order(df, 'Title'))
-print('Headline', sort_words_by_most_frequent_in_descending_order(df, 'Headline'))
-# (1) per day
-print_most_frequent_words_in_title_and_headline(df, 'PublishDate_date')
-# (1) per topic
-print_most_frequent_words_in_title_and_headline(df, 'Topic')
-
-# (2) get date, hour two column
-platform_list = ['Facebook', 'GooglePlus', 'LinkedIn']
-topic_list = ['Economy', 'Microsoft', 'Obama', 'Palestine']
-hours = 144 / 3
-days = 2
-for platform in platform_list:
-    feedback_df = concat_same_platform_df(platform, topic_list)
-    feedback_df['average_popularity_by_hour'] = feedback_df['TS144'] / hours
-    feedback_df['average_popularity_by_day'] = feedback_df['TS144'] / 2
-    feedback_df[['IDLink', 'average_popularity_by_hour']].to_csv('output/' + platform + 'by_hour' + '.csv')
-    feedback_df[['IDLink', 'average_popularity_by_day']].to_csv('output/' + platform + 'by_day' + '.csv')
-
-# # (3)
-print(df.groupby('Topic').sum())
-print(df.groupby('Topic').mean())
+print("----------------------------------------(1)------------------------------------------")
+# # (1) total
+# title_headline = 'Title'
+# word_count_dict = get_word_count_dict(df[title_headline])
+# word_count_dict = sort_word_count_dict_in_descending_order(word_count_dict)
+# print('total', title_headline, list(word_count_dict)[:5])
+#
+# title_headline = 'Headline'
+# word_count_dict = get_word_count_dict(df['Headline'])
+# word_count_dict = sort_word_count_dict_in_descending_order(word_count_dict)
+# print('total', title_headline, list(word_count_dict)[:5])
 
 
-# (4) topic
-
-def get_title_word_list(title):
-    title = str(title)
-    title_word_list = title.split(' ')
-    for i in range(len(title_word_list)):
-        title_word_list[i] = remove_ch(title_word_list[i])
-    return title_word_list
-
-
-def get_all_topic_most_word_list(df, topic_list, title_headline):
-    all_topic_most_word_list = list()  # 放所有topic 的most_word_list
-    se_topic_most_word_dict = dict()  # 放各topic 的most_word_list(100*100)
-    for topic in topic_list:
-        topic_df = df[df['Topic'] == topic]
-        se_topic_most_word_dict[topic] = sort_words_by_most_frequent_in_descending_order(topic_df, title_headline)
-        all_topic_most_word_list += se_topic_most_word_dict[topic]
-    all_topic_most_word_list = list(set(all_topic_most_word_list))
-    return all_topic_most_word_list, se_topic_most_word_dict
-
-
-def create_new_co_occurrence_matrices(most_word_list):
-    temp_list = [0 for i in range(100)]
-    co_occurrence_dict = dict()
-    co_occurrence_dict['index'] = most_word_list
-    for word in most_word_list:
-        co_occurrence_dict[word] = temp_list
-    return pd.DataFrame(co_occurrence_dict).set_index('index')
-
-
-topic_list = list(set(df['Topic'].values))
-
-# Title
-all_topic_most_word_list, se_topic_most_word_dict = get_all_topic_most_word_list(df, topic_list, 'Title')
-# title_split_list
-title_split_list = [get_title_word_list(title) for title in df['Title'].to_numpy()]
-occurrence_dict = dict()
-for word in all_topic_most_word_list:
-    occurrence_dict[word] = [1 if word in title_split else 0 for title_split in title_split_list]
-occurrence_df = pd.DataFrame(occurrence_dict)
-# print(occurrence_df)
-
-for topic in topic_list:
-    # new co_occurrence_df
-    most_word_list = se_topic_most_word_dict[topic]
-    # print(most_word_list)
-    co_occurrence_df = create_new_co_occurrence_matrices(most_word_list)
-    # print(co_occurrence_df)
-
-    # # count co_occurrence
-    # # find all 組合
-    all_product_list = list(product(most_word_list, most_word_list))
-    for com in all_product_list:
-        occurrence_number = occurrence_df[(occurrence_df[com[0]] == 1) & (occurrence_df[com[1]] == 1)][com[0]].count()
-        co_occurrence_df[com[0]][com[1]] = occurrence_number
-    print(co_occurrence_df)
-print('##########################################################################################')
-# Headline
-all_topic_most_word_list, se_topic_most_word_dict = get_all_topic_most_word_list(df, topic_list, 'Headline')
-# title_split_list
-title_split_list = [get_title_word_list(title) for title in df['Headline'].to_numpy()]
-occurrence_dict = dict()
-for word in all_topic_most_word_list:
-    occurrence_dict[word] = [1 if word in title_split else 0 for title_split in title_split_list]
-occurrence_df = pd.DataFrame(occurrence_dict)
-# print(occurrence_df)
-
-for topic in topic_list:
-    # new co_occurrence_df
-    most_word_list = se_topic_most_word_dict[topic]
-    # print(most_word_list)
-    co_occurrence_df = create_new_co_occurrence_matrices(most_word_list)
-    # print(co_occurrence_df)
-
-    # # count co_occurrence
-    # # find all 組合
-    all_product_list = list(product(most_word_list, most_word_list))
-    for com in all_product_list:
-        occurrence_number = occurrence_df[(occurrence_df[com[0]] == 1) & (occurrence_df[com[1]] == 1)][com[0]].count()
-        co_occurrence_df[com[0]][com[1]] = occurrence_number
-    print(co_occurrence_df)
-
-end_time = time.time()
-print('time', end_time - start_time, 's')
+# by_column = 'PublishDate_date'
+# title_headline = 'Title'
+# print()
+# day_set_list = list(set(df[by_column].to_list()))
+# for day in day_set_list:
+#     print(day)
+# date_df = df[df[by_column] == day][title_headline]
+# word_count_dict = get_word_count_dict(date_df)
+# word_count_dict = sort_word_count_dict_in_descending_order(word_count_dict)
+# print(day, title_headline, list(word_count_dict)[:5])
--- hw2/re_vit_hw2.py
+++ hw2/re_vit_hw2.py
@@ -1,6 +1,9 @@
 import pyspark.pandas as ps
 from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
+from pyspark.sql.types import *
+from itertools import product
+import pandas as pd
 
 
 def remove_ch(word):
@@ -9,6 +12,7 @@ def remove_ch(word):
     for ch in remove_ch_list:
         word = word.lower().replace(ch, '')
     return word
+
 
 def get_word_count_dict(df):
     word_list = df.values
@@ -26,49 +30,149 @@ def get_word_count_dict(df):
                 word_count_dict[word] = 1
     return word_count_dict
 
+
 def sort_word_count_dict_in_descending_order(word_count_dict):
     return {k: v for k, v in sorted(word_count_dict.items(), key=lambda item: item[1], reverse=True)}
+
+def print_word_count_dict_groupby_column_and_title_headline(title_headline, groupby_column='total'):
+    if groupby_column != 'total':
+        day_set_list = list(set(df[groupby_column].to_list()))
+        for day in day_set_list:
+            date_df = df[df[groupby_column] == day][title_headline]
+            word_count_dict = get_word_count_dict(date_df)
+            word_count_dict = sort_word_count_dict_in_descending_order(word_count_dict)
+            print(day, title_headline, list(word_count_dict)[:5])
+    else:
+        word_count_dict = get_word_count_dict(df[title_headline])
+        word_count_dict = sort_word_count_dict_in_descending_order(word_count_dict)
+        print('total', title_headline, list(word_count_dict)[:5])
+
+def read_csv_to_ps_df(path, sqlContext):
+    pd_df = pd.read_csv(path)
+    column_list = list(pd_df.columns)
+    schema = StructType([StructField(column, StringType(), False) for column in column_list])
+    spark_df = sqlContext.createDataFrame(pd_df, schema)
+    ps_df = spark_df.to_pandas_on_spark()
+    ps_df.dropna()
+    return ps_df
+
 
 conf = SparkConf().setAppName('hw2').setMaster("spark://10.0.2.15:7077")
 sc = SparkContext()
 sqlContext = SQLContext(sc)
 
-df = ps.read_csv('File:///opt/spark/hw2/News_Final.csv')
-df = df.dropna()
+
+df = read_csv_to_ps_df('File:///opt/spark/hw2/News_Final.csv', sqlContext)
+# df = ps.read_csv('File:///opt/spark/hw2/News_Final.csv', sep=',')
+
+'''
 # change dtype
 df['SentimentTitle'] = df['SentimentTitle'].astype('float32')
 df['SentimentHeadline'] = df['SentimentHeadline'].astype('float32')
 # PublishDate to PublishDate_date column
 all_date_list = df['PublishDate'].values
 df['PublishDate_date'] = [date_time.split(' ')[0] for date_time in all_date_list]
-print(df['PublishDate_date'])
 
 print("----------------------------------------(1)------------------------------------------")
 # (1) total
-title_headline = 'Title'
-word_count_dict = get_word_count_dict(df[title_headline])
-word_count_dict = sort_word_count_dict_in_descending_order(word_count_dict)
-print('total', title_headline, list(word_count_dict)[:5])
+print_word_count_dict_groupby_column_and_title_headline('Title')
+print_word_count_dict_groupby_column_and_title_headline('Headline')
 
-title_headline = 'Headline'
-word_count_dict = get_word_count_dict(df['Headline'])
-word_count_dict = sort_word_count_dict_in_descending_order(word_count_dict)
-print('total', title_headline, list(word_count_dict)[:5])
+# (1) day
+print_word_count_dict_groupby_column_and_title_headline('Title', 'PublishDate_date')
+print_word_count_dict_groupby_column_and_title_headline('Headline', 'PublishDate_date')
+# (1) Topic
+print_word_count_dict_groupby_column_and_title_headline('Title', 'Topic')
+print_word_count_dict_groupby_column_and_title_headline('Headline', 'Topic')
+
+print("----------------------------------------(2)------------------------------------------")
 
 
-by_column = 'PublishDate_date'
-title_headline = 'Title'
-day_set_list = list(set(df[by_column].to_list()))
-for day in day_set_list:
-    date_df = df[df[by_column] == day][title_headline]
-    word_count_dict = get_word_count_dict(date_df)
-    word_count_dict = sort_word_count_dict_in_descending_order(word_count_dict)
-    print(day, title_headline, list(word_count_dict)[:5])
+def concat_same_platform_df(platform, topic_list):
+    feedback_df = ps.read_csv('File:///opt/spark/hw2/feedback/' + platform + '_' + topic_list[0] + '.csv')
+    for i in range(1, len(topic_list)):
+        feedback_df = ps.concat([feedback_df, ps.read_csv('File:///opt/spark/hw2/feedback/' + platform + '_' + topic_list[i] + '.csv')],
+                                ignore_index=True)
+    feedback_df['IDLink'] = feedback_df['IDLink'].astype('int64')
+    return feedback_df
 
-title_headline = 'Headline'
-day_set_list = list(set(df[by_column].to_list()))
-for day in day_set_list:
-    date_df = df[df[by_column] == day][title_headline]
-    word_count_dict = get_word_count_dict(date_df)
-    word_count_dict = sort_word_count_dict_in_descending_order(word_count_dict)
-    print(day, title_headline, list(word_count_dict)[:5])
+
+platform_list = ['Facebook', 'GooglePlus', 'LinkedIn']
+topic_list = ['Economy', 'Microsoft', 'Obama', 'Palestine']
+hours = 144 / 3
+days = 2
+for platform in platform_list:
+    feedback_df = concat_same_platform_df(platform, topic_list)
+    feedback_df['TS144'] = feedback_df['TS144'].astype('float32')
+    feedback_df['average_popularity_by_hour'] = feedback_df['TS144'] / hours
+    feedback_df['average_popularity_by_day'] = feedback_df['TS144'] / days
+    feedback_df[['IDLink', 'average_popularity_by_hour']].to_csv('File:///opt/spark/hw2/output/' + platform + 'by_hour')
+    feedback_df[['IDLink', 'average_popularity_by_day']].to_csv('File:///opt/spark/hw2/output/' + platform + 'by_day')
+
+# (3)
+print("----------------------------------------(3)------------------------------------------")
+print(df.groupby('Topic').sum())
+print(df.groupby('Topic').mean())
+'''
+
+# (4) topic
+print("----------------------------------------(4)------------------------------------------")
+
+
+def get_title_word_list(title):
+    title = str(title)
+    title_word_list = title.split(' ')
+    for i in range(len(title_word_list)):
+        title_word_list[i] = remove_ch(title_word_list[i])
+    return title_word_list
+
+
+def get_all_topic_most_word_list(df, topic_list, title_headline):
+    all_topic_most_word_list = list()  # 放所有topic 的most_word_list
+    se_topic_most_word_dict = dict()  # 放各topic 的most_word_list(100*100)
+    for topic in topic_list:
+        topic_df = df[df['Topic'] == topic][title_headline]
+        se_topic_most_word_dict[topic] = list(sort_word_count_dict_in_descending_order(get_word_count_dict(topic_df)))[
+                                         :100]
+        all_topic_most_word_list += se_topic_most_word_dict[topic]
+    all_topic_most_word_list = list(set(all_topic_most_word_list))
+    return all_topic_most_word_list, se_topic_most_word_dict
+
+
+def create_new_co_occurrence_matrices(most_word_list):
+    temp_list = [0 for i in range(100)]
+    co_occurrence_dict = dict()
+    co_occurrence_dict['index'] = most_word_list
+    for word in most_word_list:
+        co_occurrence_dict[word] = temp_list
+    return pd.DataFrame(co_occurrence_dict).set_index('index')  # todo pd->ps
+
+
+def print_co_occurrence_matrices(title_headline):
+    topic_list = list(set(df['Topic'].values))
+
+    # Title
+    all_topic_most_word_list, se_topic_most_word_dict = get_all_topic_most_word_list(df, topic_list, title_headline)
+    # title_split_list
+    title_split_list = [get_title_word_list(title) for title in df[title_headline].to_numpy()]
+    occurrence_dict = dict()
+    for i in range(len(all_topic_most_word_list)):
+        occurrence_dict[all_topic_most_word_list[i]] = [1 if all_topic_most_word_list[i] in title_split else 0 for title_split in title_split_list]
+    occurrence_df = pd.DataFrame(occurrence_dict)
+    # print(occurrence_df)
+
+    for topic in topic_list:
+        # new co_occurrence_df
+        most_word_list = se_topic_most_word_dict[topic]
+        co_occurrence_df = create_new_co_occurrence_matrices(most_word_list)
+        all_product_list = list(product(most_word_list, most_word_list))
+        for com in all_product_list:
+            occurrence_number = occurrence_df[(occurrence_df[com[0]] == 1) & (occurrence_df[com[1]] == 1)][
+                com[0]].count()
+            co_occurrence_df[com[0]][com[1]] = occurrence_number
+        print(topic)
+        print(co_occurrence_df)
+
+
+print_co_occurrence_matrices('Title')
+print_co_occurrence_matrices('Headline')
