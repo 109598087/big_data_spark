6feb8fe3237ea64e8ac4c05facff157962082143
109598087 <t109598087@ntut.org.tw>
2021-12-02 19:24:57 +0800

    hw3_1 refine ok - lower

--- hw3/1.py
+++ hw3/1.py
@@ -1,77 +1,62 @@
+import re
+
 from bs4 import BeautifulSoup
 import pandas as pd
 from os import listdir
+import numpy as np
 
 
-def get_k_shingles_list(k, document):
-    shingles_2_word_list = list()
-    d_split = document.split(' ')
+def get_document_split_list(body_document):
+    body_document = body_document.replace('\n', ' ')
+    d_split = re.sub("[^\w]", " ", body_document).split()
+    return d_split
+
+
+def get_k_shingles_list(k, body_document):
+    shingles_list = list()
+    d_split = get_document_split_list(body_document)
     for i in range(len(d_split) - (k - 1)):
-        shingles_2_word_list.append([d_split[i + j] for j in range(k) if d_split[i + j] != ''])
-    result_list = list()
-    for shingles_2 in shingles_2_word_list:
-        if len(shingles_2) > 1:
-            result_list.append(shingles_2)
-    return result_list
-
-
-def remove_ch(word):
-    remove_ch_list = [',', '?', '\'', '$', ';', '.', '&', ':', '!', '#', '”', '“', '£', '/', '(', ')', '"""', '\xa0',
-                      '\x9d', '\n', '\x03']
-    for ch in remove_ch_list:
-        word = word.lower().replace(ch, '')
-    return word
+        shingles_list.append([d_split[i + j] for j in range(k) if d_split[i + j] != ''])
+    return shingles_list
 
 
 files = listdir('reuters21578/')
-# print(files)
 sgm_file_list = [file for file in files if file.endswith('.sgm')]
-print(sgm_file_list)
 
-# get_all_shingles
-all_shingle_list = list()
-all_body_list = list()
-k = 2
+# get_all_body_element
+all_body_document_list = list()
 for sgm_file in sgm_file_list:
-    print(sgm_file, end=', ')
     file_path = 'reuters21578/' + sgm_file
     document = BeautifulSoup(open(file_path, encoding="utf-8"), 'html.parser')
-
-    # get_2_shingles 1 file
     body_list = document.find_all('body')
-    print(len(body_list))
-    body_contents_list = [remove_ch(body.contents[0]) for body in body_list]
-    all_body_list += body_contents_list
-    k_2_shingles_list = get_k_shingles_list(k, body_contents_list[0])
-    all_shingle_list += k_2_shingles_list
+    body_contents_list = [body.contents[0] for body in body_list]
+    all_body_document_list += body_contents_list
+# print(all_body_document_list)
+# print(len(all_body_document_list))
 
+one_body_one_shingles_list = list()
+for body_document in all_body_document_list:
+    shingles_list = get_k_shingles_list(2, body_document)
+    one_body_one_shingles_list.append(shingles_list)
+# print(one_body_one_shingles_list)
+# print(len(one_body_one_shingles_list))
 
-# print(all_shingle)
-# print(len(all_shingle))
-# print(all_body_list)
-# print(len(all_body_list))
+all_shingles_list = list()
+for body_document in all_body_document_list:
+    shingles_list = get_k_shingles_list(2, body_document)
+    for shingles in shingles_list:
+        if shingles not in all_shingles_list:
+            all_shingles_list.append(shingles)
+# print(all_shingles_list)
+# print(len(all_shingles_list))
 
-def shingles_to_string(k, shingles):
-    str1 = ''
-    for i in range(k):
-        str1 += shingles[i]
-        if k != len(shingles) - 1:
-            str1 += ' '
-    return str1
-
-
-# MxN matrix
+# # # MxN matrix
 body_shingle_dict = dict()
-for i in range(len(all_body_list)):
-    # print(body)
-    shingle_in_body_list = [1 if shingles_to_string(k, shingles) in all_body_list[i] else 0 for shingles in
-                            all_shingle_list]
-    body_shingle_dict[str(i)] = shingle_in_body_list
-
+for i in range(len(one_body_one_shingles_list)):
+    body_shingle_dict[i] = [1 if shingles in one_body_one_shingles_list[i] else 0 for shingles in all_shingles_list]
 df = pd.DataFrame(body_shingle_dict)
-# df['index'] = all_shingle_list
-df.insert(0, 'index', all_shingle_list)
-print(df)
-df.to_csv('hw3_1.csv', index=False)
+df.insert(0, 'index', all_shingles_list)
 
+print(df)
 print(df.sum())
+df.to_csv('hw3_1.csv', index=False)
