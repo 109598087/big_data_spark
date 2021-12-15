4e7b91b9cf395eeedafc2b0b59f8704cbfd24c45
109598087 <t109598087@ntut.org.tw>
2021-12-15 20:01:11 +0800

    hw4_5 ok

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -109,42 +109,66 @@ def get_cosine_similarity(a_np, b_np):
     return np.sum(a_np * b_np) / (pow(np.sum(np.square(a_np)), 1 / 2) * pow(np.sum(np.square(b_np)), 1 / 2))
 
 
-# user_id = int(input('Please input UserID: '))
-user_id = 50
-
-cosine_similarity_list = list()
-all_user_np = users_df['UserID'].to_numpy()
-print(all_user_np)
-
-for user_id2 in all_user_np:
-    two_user_df = pd.DataFrame()
-    two_user_df['MovieID'] = movies_df['MovieID']
-    two_user_df = two_user_df.set_index('MovieID')
-
-    user1_df = ratings_df[ratings_df['UserID'] == user_id].set_index('MovieID')
-    user2_df = ratings_df[ratings_df['UserID'] == user_id2].set_index('MovieID')
-
-    two_user_df = two_user_df.merge(user1_df, how='outer', left_index=True, right_index=True)
-    two_user_df = two_user_df.merge(user2_df, how='outer', left_index=True, right_index=True)
-    two_user_df = two_user_df.fillna(0)
-    # print(two_user_df)
-
-    a_np = two_user_df['Rating_x'].to_numpy()
-    b_np = two_user_df['Rating_y'].to_numpy()
-
-    cosine_similarity_list.append(get_cosine_similarity(a_np, b_np))
-
-# print(cosine_similarity_list)
-
-user_cosine_similarity_df = pd.DataFrame({
-    'UserID': all_user_np,
-    'score': cosine_similarity_list,
-}).sort_values('score', ascending=False)
-
-user_cosine_similarity_df.to_csv('output/user_cosine_similarity.csv', index=False)
+# # user_id = int(input('Please input UserID: '))
+# user_id = 50
+#
+# user_cosine_similarity_list = list()
+# all_user_np = users_df['UserID'].to_numpy()
+# print(all_user_np)
+#
+# for user_id2 in all_user_np:
+#     two_user_df = pd.DataFrame()
+#     two_user_df['MovieID'] = movies_df['MovieID']
+#     two_user_df = two_user_df.set_index('MovieID')
+#
+#     user1_df = ratings_df[ratings_df['UserID'] == user_id].set_index('MovieID')
+#     user2_df = ratings_df[ratings_df['UserID'] == user_id2].set_index('MovieID')
+#
+#     two_user_df = two_user_df.merge(user1_df, how='outer', left_index=True, right_index=True)
+#     two_user_df = two_user_df.merge(user2_df, how='outer', left_index=True, right_index=True)
+#     two_user_df = two_user_df.fillna(0)
+#     # print(two_user_df)
+#
+#     a_np = two_user_df['Rating_x'].to_numpy()
+#     b_np = two_user_df['Rating_y'].to_numpy()
+#
+#     user_cosine_similarity_list.append(get_cosine_similarity(a_np, b_np))
+#
+# # print(cosine_similarity_list)
+#
+# user_cosine_similarity_df = pd.DataFrame({
+#     'UserID': all_user_np,
+#     'score': user_cosine_similarity_list,
+# }).sort_values('score', ascending=False)
+#
+# user_cosine_similarity_df.to_csv('output/user_cosine_similarity.csv', index=False)
 ##########################################################################
 # 5
+# movie_id = int(input('Please input MovieID: '))
 movie_id = 50
 all_movie_np = movies_df['MovieID'].to_numpy()
-print(all_user_np)
+movie_cosine_similarity_list = list()
+print(all_movie_np)
+for movie_id2 in all_movie_np:
+    two_movie_df = pd.DataFrame()
+    two_movie_df['UserID'] = users_df['UserID']
+    two_movie_df = two_movie_df.set_index('UserID')
 
+    movie1_df = ratings_df[ratings_df['MovieID'] == movie_id].set_index('UserID')
+    movie2_df = ratings_df[ratings_df['MovieID'] == movie_id2].set_index('UserID')
+
+    two_movie_df = two_movie_df.merge(movie1_df, how='outer', left_index=True, right_index=True)
+    two_movie_df = two_movie_df.merge(movie2_df, how='outer', left_index=True, right_index=True)
+    two_movie_df = two_movie_df.fillna(0)
+
+    a_np = two_movie_df['Rating_x'].to_numpy()
+    b_np = two_movie_df['Rating_y'].to_numpy()
+
+    movie_cosine_similarity_list.append(get_cosine_similarity(a_np, b_np))
+
+movie_cosine_similarity_df = pd.DataFrame({
+    'UserID': all_movie_np,
+    'score': movie_cosine_similarity_list,
+}).sort_values('score', ascending=False)
+
+movie_cosine_similarity_df.to_csv('output/movie_cosine_similarity.csv', index=False)
