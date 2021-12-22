10cbb50cab332cf75478edc8015d0311f61e0401
109598087 <t109598087@ntut.org.tw>
2021-12-22 19:46:23 +0800

    hw5 1 2 3ok

--- hw5/hw5.py
+++ hw5/hw5.py
@@ -5,10 +5,18 @@ google_df.columns = ['FromNodeId', 'ToNodeId']
 
 # 1
 outlinks_df = google_df.groupby('FromNodeId').count().sort_values('ToNodeId', ascending=False)
-outlinks_df.to_csv('outlinks.csv')
+outlinks_df.to_csv('output/outlinks.csv')
 
 # 2
 inlinks_df = google_df.groupby('ToNodeId').count().sort_values('FromNodeId', ascending=False)
-inlinks_df.to_csv('inlinks.csv')
+inlinks_df.to_csv('output/inlinks.csv')
 
 # 3
+v = 0
+from_v_df = google_df[google_df['FromNodeId'] == v]
+print(from_v_df)
+from_v_df.to_csv('output/from_v.csv', index=False)
+
+to_v_df = google_df[google_df['ToNodeId'] == v]
+print(to_v_df)
+to_v_df.to_csv('output/to_v.csv', index=False)
